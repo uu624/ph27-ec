01ab026088d716a242f30bca6998ece2b6ade36a
conn-do <kondo@japaan.jp>
2025-06-12 18:35:13 +0900

    2025-06-12 授業内容

---
+++ resources/views/sample.blade.php
@@ -0,0 +1,7 @@
+<html>
+
+<body>
+    <h1>Sample</h1>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\SampleController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
+
+Route::get('/sample', [SampleController::class, 'index']);
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
