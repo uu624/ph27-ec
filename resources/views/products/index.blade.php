a1bcbe61a9a52b85272406cca81aad046b0b43c4
conn-do <kondo@japaan.jp>
2025-06-26 01:04:20 +0900

    query builder

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -11,4 +11,14 @@
             </li>
         @endforeach
     </ul>
+
+    <h2 class="sale">特価商品</h2>
+    <ul>
+        @foreach ($saleProducts as $product)
+            <li>
+                <a href="/products/{{ $product->id }}">{{ $product->name }}</a>
+                {{ $product->price }} 円
+            </li>
+        @endforeach
+    </ul>
 @endsection
